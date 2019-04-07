99e5ad929cd6ff597cda8586b594a41af3802b15
Devan0369 <alifesource@gmail.com>
2019-04-07 23:43:49 +0800

    textToSpeech

---
+++ textToSpeech001.py
@@ -0,0 +1,14 @@
+#Note you must have installed gTTS
+#command for installation
+#pip install gTTS
+
+from gtts import gTTS
+text=""
+with open("tts.txt","r") as file:
+    for line in file:
+        text=text + line
+
+
+speech=gTTS(text,'en','slow')
+
+speech.save("hello.mp3")
